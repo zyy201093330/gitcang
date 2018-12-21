93433cb42c0437408e641041c149f58394a1b6d4
zhangyy201093330 <m15712879594@163.com>
2018-12-21 15:07:40 +0800

    cz

--- musics/src/routes/video/Index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class Index extends React.PureComponent{
-  render(){
-    return <h1>{this.props.match.path}</h1>
-  }
-}
-
-export default Index;
--- musics/src/utils/user.js
+++
@@ -1,9 +0,0 @@
-import cookie from 'js-cookie';
-
-export function getToken(){
-  return cookie.get('auth_token');
-}
-
-export function setToken(val){
-  cookie.set('auth_token', val);
-}
