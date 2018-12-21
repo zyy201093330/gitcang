074f7f92516c3633b38cd0ae9b0f617e0234ffb8
zhangyy201093330 <m15712879594@163.com>
2018-12-21 08:03:56 +0800

    ant

---
+++ Ant/config/yuedu/index.js
@@ -0,0 +1,12 @@
+export default{
+    path:'/shop',
+    name:'商店',
+    icon:'book',
+    routes:[
+        {
+            path:'/shop/first',
+            name:'商店列表',
+            component:'./Shop/index.js'
+        }
+    ],
+}
